ebe09ad1ec2be858c2d59aacdbd19e9edf170a7c
tanujjh123 <tanujjh123@gmail.com>
2024-12-30 13:43:56 +0530

    Updated config file
    Signed-off-by: tanujjh123 <tanujjh123@gmail.com>

--- TestScripts/test_DataLoading.py
+++ TestScripts/test_DataLoading.py
@@ -20,6 +20,9 @@ import os
 if not os.path.exists('Logs'):
     os.makedirs('Logs')
 
+if not os.path.exists('LoadingErrors'):
+        os.makedirs('LoadingErrors')
+
 # Logging mechanism
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -38,7 +41,7 @@ def test_fact_sales_table_load():
     try:
         query_expected = """select sales_id,product_id,store_id,quantity,total_amount as total_sales ,sale_date from sales_with_deatils order by sales_id,product_id,store_id ;"""
         query_actual = """select sales_id,product_id,store_id,quantity,total_sales,sale_date from fact_sales order by sales_id,product_id,store_id ;"""
-        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine)
+        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine,'LoadingErrors/fact_Sales_differences.csv')
         logger.info("test_fact_sales_table_load test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -52,7 +55,7 @@ def test_monthly_sales_summary_table_load():
     try:
         query_expected = """select * from  monthly_sales_summary_source order by product_id,month,year;"""
         query_actual = """select * from  monthly_sales_summary order by product_id,month,year;"""
-        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine)
+        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine,'LoadingErrors/Monthly_Sales_Differances.csv')
         logger.info("test_monthly_sales_summary_table_load test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -65,7 +68,7 @@ def test_inventory_fact_load():
     try:
         query_expected = """select * from  staging_inventory order by product_id,store_id;"""
         query_actual = """select * from  fact_inventory order by product_id,store_id;"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'LoadingErrors/inventory_Fact_differences.csv')
         logger.info("test_inventory_fact_load test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -79,7 +82,7 @@ def test__inventory_levels_by_store_fact_load():
     try:
         query_expected = """select * from  aggregated_inventory_levels order by store_id;"""
         query_actual = """select store_id,cast(total_inventory as Double) as total_inventory from inventory_levels_by_store;"""
-        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine)
+        db_to_db_verify(query_expected,mysql_engine,query_actual,mysql_engine,'LoadingErrors/Inventory_Levels_By_Store_differences.csv')
         logger.info("test__inventory_levels_by_store_fact_load test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
--- TestScripts/test_DataTransfromation.py
+++ TestScripts/test_DataTransfromation.py
@@ -32,7 +32,7 @@ def test_filter_transformation():
     try:
         query_expected = """select * from staging_sales where sale_date <='2024-09-20'"""
         query_actual = """select * from filtered_sales"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Filter_Transformation_Error.csv')
         logger.info("Filter transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -44,7 +44,7 @@ def test_Router_High_region_sales_transformation():
     try:
         query_expected = """select * from filtered_sales where region='High'"""
         query_actual = """select * from high_sales"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Router_data_Transformation_differences.csv')
         logger.info("test_Router_High_region_sales_transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -56,7 +56,7 @@ def test_Router_Low_region_sales_transformation():
     try:
         query_expected = """select * from filtered_sales where region='Low'"""
         query_actual = """select * from low_sales"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Low_Region_Sales_Transformation_differences.csv')
         logger.info("test_Router_Low_region_sales_transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -67,7 +67,7 @@ def test_Aggregate_Sales_data_transformation():
     try:
         query_expected = """select product_id, month(sale_date) as month, year(sale_date) as year, sum(quantity * price) as total_sales from filtered_sales group by product_id, month(sale_date), year(sale_date)"""
         query_actual = """select * from monthly_sales_summary_source"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Aggregate_Sales_Data_differences.csv')
         logger.info("test_Aggregate_Sales_data_transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -78,7 +78,7 @@ def test_JOINER_transformation():
     try:
         query_expected = """select s.sales_id, s.product_id, s.store_id, p.product_name, st.store_name, s.quantity, s.price * s.quantity as total_amount, s.sale_date from filtered_sales as s join staging_product as p on s.product_id = p.product_id join staging_stores as st on s.store_id = st.store_id"""
         query_actual = """select * from sales_with_deatils"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Joiner_Transformation_differences.csv')
         logger.info("test_JOINER_transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
@@ -89,7 +89,7 @@ def test_aggregate_inventory_levels_transformation():
     try:
         query_expected = """select store_id, sum(quantity_on_hand) as total_inventory from staging_inventory group by store_id"""
         query_actual = """select * from aggregated_inventory_levels"""
-        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine)
+        db_to_db_verify(query_expected, mysql_engine, query_actual, mysql_engine,'DBErrors/Aggregare_Levels_Transformation_differences.csv')
         logger.info("test_aggregate_inventory_levels_transformation test has completed .......")
     except Exception as e:
         logger.error(f"Error occured during data transformation: {e}")
